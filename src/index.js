ef93e0b2b25fcd43c9c9842e76ad0531a6cd5f48
Joshua Byrd <phocks@gmail.com>
2020-07-03 12:02:20 +1000

    Refactor and remove extraneus

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import base36Interactives from "./module";
+import loadScripts from "@abcnews/hash-scripts-loader";
 
 // Only work once Odyssey loads (due to <a name=> links)
 if (window.__ODYSSEY__) {
-  base36Interactives(window.__ODYSSEY__);
+  loadScripts(window.__ODYSSEY__);
 } else {
   window.addEventListener("odyssey:api", e => {
-    base36Interactives(e.detail);
+    loadScripts(e.detail);
   });
 }
--- src/lib/attach.js
+++
@@ -1,6 +0,0 @@
-// Function to load script into page
-export const attach = url => {
-  const scriptTag = document.createElement("script");
-  scriptTag.setAttribute("src", url);
-  document.head.appendChild(scriptTag);
-};
