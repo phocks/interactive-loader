1b34582c1da3ca06ff21bbc0777393497e7da6bf
Joshua Byrd <phocks@gmail.com>
2021-03-24 15:50:33 +1000

    Remove Odyssey deps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import loadScripts from "@abcnews/hash-scripts-loader";
 
 // Only work once Odyssey loads (due to <a name=> links)
-if (window.__ODYSSEY__) {
-  loadScripts(window.__ODYSSEY__);
-} else {
-  window.addEventListener("odyssey:api", e => {
-    loadScripts(e.detail);
-  });
-}
+// DOESN'T REQUIRE ODYSSEY NOW PRESENTATION LAYER HANDLES
+// TRANSFORMING #HASHES TO <div id=blah
+
+// if (window.__ODYSSEY__) {
+//   loadScripts(window.__ODYSSEY__);
+// } else {
+//   window.addEventListener("odyssey:api", e => {
+//     loadScripts(e.detail);
+//   });
+// }
+
+loadScripts();
